0fcd1b77491f3da01645cc1ed1a211252c49e666
Junior Futa <juniorfuta94@gmail.com>
2023-08-16 04:22:31 +0200

    update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("navbar").style.marginLeft = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
   }
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("navbar").style.marginLeft= "0";
+    document.getElementById("main").style.marginLeft= "0";
   }
