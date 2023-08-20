dc3c3d6367ee74a24c7075809781b58777152386
Junior Futa <juniorfuta94@gmail.com>
2023-08-20 18:28:55 +0200

    fonction copied imcomplet

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
   }
+
+  function copied () {
+    document.getElementById("btn1").style.color= "green";
+    document.getElementById("btn1").style.border= " solid 2px green";
+    let icon =  document.getElementById("i1")
+    icon.style.color= "green";
+    let text = document.getElementById("btn1");
+    text.innerHTML= "COPIED";
+    let result;
+    result= icon + text; 
+    return result; 
+  }
