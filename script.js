cf10d3cb88ddd3797f75aa5083ef20dd582c70f8
Junior Futa <juniorfuta94@gmail.com>
2023-08-21 19:26:19 +0200

    definite version1 button copied

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
 
-  function copied () {
-    document.getElementById("btn1").style.color= "green";
-    document.getElementById("btn1").style.border= " solid 2px green";
-    let icon =  document.getElementById("i1")
-    icon.style.color= "green";
-    let text = document.getElementById("btn1");
-    text.innerHTML= "COPIED";
-    let result;
-    result= icon + text; 
-    return result; 
-  }
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+function copied1() {
+  document.getElementById("btn1").style.color = "green";
+  document.getElementById("btn1").style.border = " solid 2px green";
+  let icon = document.getElementById("i1")
+  icon.style.color = "green";
+  let text = document.getElementById("btn1");
+  text.innerHTML = "COPIED";
+}
+
+function copied2() {
+  document.getElementById("btn2").style.color = "green";
+  document.getElementById("btn2").style.border = "solid 2px green";
+  let icon = document.getElementById("i2")
+  icon.style.color = "green";
+  let text = document.getElementById("btn2");
+  text.innerHTML = "COPIED";
+}
+
+let textInput = document.getElementById ('numberInput');
+let textOut= document.getElementById ('textOut');
+
+textInput.addEventListener ('input', function(){
+    textOut.textContent= 'you tiped:' + textInput.value;
+  })
+
