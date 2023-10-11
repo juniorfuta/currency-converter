926e898a4ec6c3287fb507e2b4d52d67c54b9ccd
Junior Futa <juniorfuta94@gmail.com>
2023-10-11 23:00:48 +0200

    update script.js

--- js/script.js
+++ js/script.js
@@ -145,48 +145,6 @@ window.onclick = function (event) {
   }
 }
 
-//Functions to Translate
-
-//Function test to translate change language
-
-function testTranslate(language) {
-
-  let headingTranslate;
-
-  let buttonCopy1Translate;
-  let buttonCopy2Translate;
- //let iconButtonCopy;
-  let keybordTranslation;
-
-  headingTranslate = document.getElementById("heading");
-
-  buttonCopy1Translate=document.getElementById("btn1");
-  buttonCopy2Translate=document.getElementById("btn2");
-  keybordTranslation=document.getElementById("keybordHeading");
-  //iconButtonCopy=document.getElementById("i2");
-
-  if (language === "pt") {
-    headingTranslate.innerHTML = "Conversor de Moedas";
-    buttonCopy1Translate.innerHTML ="COPIAR" 
-    buttonCopy2Translate.innerHTML ="COPIAR"
-    keybordHeading.innerHTML ="Teclado"
-
-
-  }
-  else if (language === "fr") {
-    headingTranslate.innerHTML = "Convertisseur de Devises";
-    buttonCopy1Translate.innerHTML ="COPIER" 
-    buttonCopy2Translate.innerHTML ="COPIER"
-    keybordHeading.innerHTML ="Clavier" 
-  }
-  else {
-    headingTranslate.innerHTML = "Currency Converter";
-    buttonCopy1Translate.innerHTML ="COPY" 
-    buttonCopy2Translate.innerHTML ="COPY" 
-    keybordHeading.innerHTML ="Keybord"
-  }
-
-}
 
 //function to copy a text
 
