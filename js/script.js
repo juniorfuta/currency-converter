a6570c969544058c967ab436084563d9f703caf5
Junior Futa <juniorfuta94@gmail.com>
2023-10-11 22:42:50 +0200

    update script.js

--- js/script.js
+++ js/script.js
@@ -153,17 +153,29 @@ function testTranslate(language) {
 
   let headingTranslate;
 
+  let buttonCopy1Translate;
+  let buttonCopy2Translate;
+
   headingTranslate = document.getElementById("heading");
+
+  buttonCopy1Translate=document.getElementById("btn1");
+  buttonCopy2Translate=document.getElementById("btn2");
 
   if (language === "pt") {
     headingTranslate.innerHTML = "Conversor de Moedas";
+    buttonCopy1Translate.innerHTML ="COPIAR" 
+    buttonCopy2Translate.innerHTML ="COPIAR" 
 
   }
   else if (language === "fr") {
     headingTranslate.innerHTML = "Convertisseur de Devises";
+    buttonCopy1Translate.innerHTML ="COPIER" 
+    buttonCopy2Translate.innerHTML ="COPIER" 
   }
   else {
     headingTranslate.innerHTML = "Currency Converter";
+    buttonCopy1Translate.innerHTML ="COPY" 
+    buttonCopy2Translate.innerHTML ="COPY" 
   }
 
 }
