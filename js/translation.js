0eaee2eda00c658e4b12c90b17939b2635388c94
Junior Futa <juniorfuta94@gmail.com>
2023-10-14 22:49:41 +0200

    update translation.js

--- js/translation.js
+++ js/translation.js
@@ -125,5 +125,6 @@ function testTranslate(language) {
     darkTranslate.innerHTML="Dark"
     lightTranslate.innerHTML="Light"
     aboutTranslate.innerHTML="About Converter"
+    aboutcontainerTranslate.innerHTML="PalancaTech's currency converter allows you to check the latest average bid/ask exchange rates and convert all major world currencies. PalancaTech Rates are compiled exchange rates from major market data sources"
   }
 }
