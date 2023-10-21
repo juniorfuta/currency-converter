54267a2119ff123ccb7343f1c68c3d53b545e8a9
Junior Futa <juniorfuta94@gmail.com>
2023-10-21 13:34:21 +0200

    update translation.js

--- js/translation.js
+++ js/translation.js
@@ -161,6 +161,7 @@ function PolicyTranslate(language) {
   let policyHeadingTranslate = document.getElementById("policyHeading");
   let cookiesContentTranslate = document.getElementById("cookiesPolicyContent");
   let concentHeadingTranslate = document.getElementById("concentHeading");
+  let concentContentTranslate = document.getElementById("consentPolicyContent");
 
   //iconButtonCopy=document.getElementById("i2");
 
@@ -168,18 +169,21 @@ function PolicyTranslate(language) {
     policyHeadingTranslate.innerHTML = "Políticas de Privacidade"
     cookiesContentTranslate.innerHTML = "No nosso website não recolhemos quaisquer dados pessoais apenas ideias do que procura."
     concentHeadingTranslate.innerHTML = "2.Consentimento"
+    
   }
 
   else if (language === "fr") {
     policyHeadingTranslate.innerHTML = "Politiques de Confidentialité"
     cookiesContentTranslate.innerHTML = "Sur notre site Web, nous ne collectons aucune donnée personnelle, juste des idées sur ce que vous recherchez."
     concentHeadingTranslate.innerHTML = "2.Consentement"
+    concentContentTranslate.innerHTML = "En utilisant notre site Web, vous acceptez notre politique de confidentialité."
   }
 
   else {
     policyHeadingTranslate.innerHTML = "Privacy Policies"
     cookiesContentTranslate.innerHTML = "In our website we don't collect  any  personnal data just ideias of what you search."
     concentHeadingTranslate.innerHTML = "2.Consent"
+    concentContentTranslate.innerHTML = "Using our website you are you are agreeing to our privacy policy."
   }
 }
 
