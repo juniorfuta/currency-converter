524d4cd5e9dedec53e51c62751e99493d95ec9b1
Junior Futa <juniorfuta94@gmail.com>
2023-10-21 14:19:29 +0200

    update translation.js

--- js/translation.js
+++ js/translation.js
@@ -193,6 +193,7 @@ function termsTranslate(language){
   let termsHeadingTranslate = document.getElementById("termsHeading");
   let p1Translate = document.getElementById("p1");
   let p2Translate = document.getElementById("p2");
+  let p3Translate = document.getElementById("p2");
 
   //iconButtonCopy=document.getElementById("i2");
 
@@ -200,17 +201,20 @@ function termsTranslate(language){
     termsHeadingTranslate.innerHTML = "Termos de Uso"
     p1Translate.innerHTML = "<b>1.Uso pessoal e não comercial:</b> este site não tem fins lucrativos e é para uso pessoal."
     p2Translate.innerHTML = "<b>2.Precisão das taxas de câmbio: </b>as taxas de câmbio exibidas são apenas indicativas mas com precisão garantida."
+    p3Translate.innerHTML = "<b>2.Precisão das taxas de câmbio: </b>as taxas de câmbio exibidas são apenas indicativas mas com precisão garantida."
   }
 
   else if (language === "fr") {
     termsHeadingTranslate.innerHTML = "Conditions d'Utilisation"
     p1Translate.innerHTML = "<b>1.Utilisation personnelle et non commerciale:</b> ce site Web est à but non lucratif et destiné à un usage personnel."
     p2Translate.innerHTML = "<b>2.Précision des taux de change: </b>les taux de change affichés ne sont qu'indicatifs mais avec une précision garantie."
+    p3Translate.innerHTML = "<b>2.Precisão das taxas de câmbio: </b>as taxas de câmbio exibidas são apenas indicativas mas com precisão garantida."
   }
 
   else {
     termsHeadingTranslate.innerHTML = "Terms de Use"
     p1Translate.innerHTML = "<b>1.Personal and non-commercial use:</b> this website is non-profit and for personal use."
     p2Translate.innerHTML = "<b>2.Accuracy of exchange rates: </b>the exchange rates displayed are only indicative but with guaranteed accuracy."
+    p3Translate.innerHTML = "<b>3.Intellectual property:</b> all intellectual property rights related to the tool(including software, design and contents) remain our property."
   }
 }
