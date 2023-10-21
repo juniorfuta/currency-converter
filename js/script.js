19a4c297f6dabbfa83062a0dbb8dae09f00a14cc
Junior Futa <juniorfuta94@gmail.com>
2023-10-21 17:53:34 +0200

    update script.js and theme_light.css

--- js/script.js
+++ js/script.js
@@ -184,11 +184,13 @@ function toggleTheme() {
 function darkTheme() {
   const body=document.body
   body.setAttribute ("data-theme", "dark")
+  localStorage.setItem("data-theme", "dark")
 }
 
 function lightTheme() {
   const body=document.body
   body.setAttribute ("data-theme", "light")
+  localStorage.setItem("data-theme", "light")
 }
 
 /*
