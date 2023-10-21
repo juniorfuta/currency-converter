8edfeb54f5f8cc5b83a0035ef91bb05657c8cd73
Junior Futa <juniorfuta94@gmail.com>
2023-10-21 17:56:49 +0200

    update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,1 +1,13 @@
-alert('Ok')
+//Change background color 
+
+function darkTheme() {
+  const body=document.body
+  body.setAttribute ("data-theme", "dark")
+  localStorage.setItem("data-theme", "dark")
+}
+
+function lightTheme() {
+  const body=document.body
+  body.setAttribute ("data-theme", "light")
+  localStorage.setItem("data-theme", "light")
+}
