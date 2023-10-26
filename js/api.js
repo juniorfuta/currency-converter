7e1d2b940c57fe27ab4f701ac11071fe1a15144f
Junior Futa <juniorfuta94@gmail.com>
2023-10-26 20:53:50 +0200

    update api.js

--- js/api.js
+++ js/api.js
@@ -6,36 +6,10 @@ E-mail: juniorfuta94@gmail.com
 Instagram: @juniorfuta
 */
 
+//Integrating API
 
-var requestURL = 'https://api.exchangerate.host/latest'; 
-var request = new XMLHttpRequest(); 
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
+fetch ('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur.json')
+.then((res) => res.json()).then ((data) => console.log(data.eur.aoa))
 
-request.onload = function() {
-  var response = request.response;
-  var data= response.rates.AOA;
-  console.log(data);
-}
-
-//Methode fetch
-const data = await (await fetch ('https://api.exchangerate.host/latest')). json();
-console.log(data);
-
-
-const API_URL= "https://api.exchangerate.host/"
-const API_CURRENCIES_RATES= "latest"
-const currenciesJSON= fetch (API_URL + API_CURRENCIES_RATES)
-const currenciesObject = currenciesJSON.json()
-console.log(currenciesObject)
-
-async function api() {
-    const requestURL =
-      "https://api.exchangerate.host/latest";
-    const request = new Request(requestURL);
-  
-    const response = await fetch(request);
-    const s = await response.json();
-}
-
+fetch ('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json')
+.then((res) => res.json()).then ((data) => console.log(data.aoa))
