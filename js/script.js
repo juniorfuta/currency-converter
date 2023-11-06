b01c75e0bb63332e0bbd73f0f88d52f644962f30
Junior Futa <juniorfuta94@gmail.com>
2023-11-06 23:23:03 +0100

    update script.js

--- js/script.js
+++ js/script.js
@@ -110,46 +110,7 @@ button.addEventListener("click", function () {
 });
 
 
-//functions for the buttons copy when clicked
 
-
-function copied1() {
-
-  document.getElementById("btn1").style.color = "green"
-
-  document.getElementById("btn1").style.border = " solid 2px green"
-
-  let value = localStorage.getItem("locale");
-
-  if (value === 'pt') {
-    document.getElementById("btn1").innerHTML = 'COPIADO <i class ="fa-solid fa-copy"></i>'
-  }
-  else if (value === 'fr') {
-    document.getElementById("btn1").innerHTML = 'COPIÉ <i class ="fa-solid fa-copy"></i>'
-  }
-  else {
-    document.getElementById("btn1").innerHTML ='COPIED <i class ="fa-solid fa-copy"></i>'
-  }
-}
-
-function copied2() {
-
-  document.getElementById("btn2").style.color = "green"
-
-  document.getElementById("btn2").style.border = "solid 2px green"
-
-  let value = localStorage.getItem("locale");
-
-  if (value === 'pt') {
-    document.getElementById("btn2").innerHTML = 'COPIADO <i class ="fa-solid fa-copy"></i>'
-  }
-  else if (value === 'fr') {
-    document.getElementById("btn2").innerHTML = 'COPIÉ <i class ="fa-solid fa-copy"></i>'
-  }
-  else {
-    document.getElementById("btn2").innerHTML = 'COPIED <i class ="fa-solid fa-copy"></i>'
-  }
-}
 
 //function for the button copy when clicked out 
 
