a1110e9c20784da957c94c7d1c83e2f4e15d0d60
Junior Futa <juniorfuta94@gmail.com>
2023-11-06 22:22:22 +0100

    update keybord.js

--- js/keybord.js
+++ js/keybord.js
@@ -12,6 +12,17 @@ function writeInput(value) {
     input1.value = newValue;
   })
 
+  input2.addEventListener('focus', function() {
+
+    let input2 = document.getElementById('input2');
+
+    const currentValue = input2.value;
+  
+    const newValue = currentValue + value;
+  
+    input2.value = newValue;
+  })
+
 }
 
 function cleanInput() {
