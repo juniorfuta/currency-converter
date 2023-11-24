44eae02b32644814737c51703181560b3b0a12bd
Junior Futa <juniorfuta94@gmail.com>
2023-11-24 23:38:58 +0100

    update api.js

--- js/api.js
+++ js/api.js
@@ -62,33 +62,7 @@ async function convertion() {
   userchoice_two.addEventListener("change", retriveRate)
 }
 
-function hystoryDirect() {
 
-  input1Select = localStorage.getItem('valueSelectOne')
-
-  input2Select = localStorage.getItem('valueSelectTwo')
-
-  inputselect1 = input1Select.toUpperCase()
-
-  inputselect2 = input2Select.toUpperCase()
-
-  inputvl1 = input1Value.value
-
-  inputvl10 = Math.round(inputvl1)
-
-  inputvl100 = inputvl10.toLocaleString()
-
-  inputvl2 = input2Value.value
-
-  inputvl20 = Math.round(inputvl2)
-
-  inputvl200 = inputvl20.toLocaleString()
-
-  if (input1Select != '' && input2Select != '') {
-    history.innerHTML += '<p>' + inputvl100 + ' ' + '<span class ="divisesNames">' + inputselect1 + '</span>' +
-      ' = ' + inputvl200 + ' ' + '<span class ="divisesNames">' + inputselect2 + '</span>' + '</p>'
-  }
-}
 
 function clearLocalStorage() {
 
